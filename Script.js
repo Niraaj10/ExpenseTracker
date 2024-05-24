cd866eadab4e8df32f4301d559add98a709f7acd
Niraaj10 <nitcode10@gmail.com>
2024-05-24 19:10:21 +0530

    Updated some logic

--- Script.js
+++ Script.js
@@ -1,31 +1,79 @@
 const expenseForm = document.getElementById('expenseForm');
 const expenseList = document.getElementById('expenseList');
 
+let totalAmount = 0;
+
 expenseForm.addEventListener('submit', function(event){
     event.preventDefault();
-
+    
     const description = document.getElementById('description').value;
     const category = document.getElementById('category').value;
     let amount = document.getElementById('amount').value;
-
+    
+    amount = parseFloat(amount);
     let total = document.getElementById('totall');
-    // total.innerHTML = amount++;
+    
+    
 
     if (document && category && !isNaN(amount)) {
-        const newRow = document.createElement('tr');
+        const currentDate = new Date();
+        const formattedDate = currentDate.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
 
-        newRow.innerHTML = `<td>${description}</td>
-        <td>${category}</td>
-        <td>${amount}</td>`;
+        totalAmount += amount;
+        total.innerHTML = totalAmount;
 
-        // total.innerHTML = amount;
+        // console.log(category);
+        
 
-        expenseList.appendChild(newRow);
+        if (document.getElementById('category').value == "Food") {
+            // document.getElementById('FoodDes').innerHTML = description;
+            // document.getElementById('FoodAmt').innerHTML =  "₹"+amount;
+            // document.getElementById('FoodDate').innerHTML = formattedDate;
 
-        document.getElementById('description').value = '';
-        document.getElementById('category').value = '';
-        document.getElementById('amount').value = '';
-    } else {
-        alert("Please fill out all the feilds with valid data");
+            document.getElementById("Food").innerHTML = `<div class="cap">
+            <div class="desc" id="FoodDes"> ${description}</div>
+            <div class="amt" id="FoodAmt"> ₹. ${amount}  <span class="date" id="FoodDate"> </span></div>
+        </div>`;
+        
+        } else if (document.getElementById('category').value == "Travel") {
+            document.getElementById('TravelDes').innerHTML = description;
+            document.getElementById('TravelAmt').innerHTML =  "₹"+amount;
+            document.getElementById('TravelDate').innerHTML = formattedDate;
+        } else if (document.getElementById('category').value == "Shopping") {
+            document.getElementById('ShoppingDes').innerHTML = description;
+            document.getElementById('ShoppingAmt').innerHTML =  "₹"+amount;
+            document.getElementById('ShoppingDate').innerHTML = formattedDate;
+        } else if (document.getElementById('category').value == "Hospital") {
+            document.getElementById('HospitalDes').innerHTML = description;
+            document.getElementById('HospitalAmt').innerHTML =  "₹"+amount;
+            document.getElementById('HospitalDate').innerHTML = formattedDate;
+        } else {
+            alert("Please fill out all the feilds with valid data");
+        }
+
+
+
+
+    //     const newRow = document.createElement('tr');
+
+    //     newRow.innerHTML = `<td>${description}</td>
+    //     <td>${category}</td>
+    //     <td>${amount}</td>
+    //     <td>${formattedDate}</td>
+    //     `;
+
+
+
+    //     expenseList.appendChild(newRow);
+
+    //     document.getElementById('description').value = '';
+    //     document.getElementById('category').value = '';
+    //     document.getElementById('amount').value = '';
+    // } else {
+    //     alert("Please fill out all the feilds with valid data");
     }
-})
+});
